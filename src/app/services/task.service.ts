0c7ed2ba9e15dbdce4263e307168437036e298cf
shubhamAsolkar22 <shubham.asolkar21@gmail.com>
2021-10-30 21:11:11 +0530

    created dummy backend rest api with json-server nad used httpClient/httpHeader service to fetch  tasks from backend.

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { TASKS } from '../../assets/mock-tasks';
 import { Task } from '../interfaces/Task';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private httpClient : HttpClient ) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.httpClient.get<Task[]>(this.apiUrl);
   }
 }
